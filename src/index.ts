bb7a5b525b6aecdf8b37f3f8136d9dc2b46635f8
nab138 <56705177+nab138@users.noreply.github.com>
2024-02-04 15:43:03 -0500

    Change min window size

--- src/index.ts
+++ src/index.ts
@@ -21,8 +21,8 @@ const createWindow = (): void => {
   const mainWindow = new BrowserWindow({
     height: 600,
     width: 800,
-    minHeight: 300,
-    minWidth: 400,
+    minHeight: 525,
+    minWidth: 750,
     webPreferences: {
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY,
     },
